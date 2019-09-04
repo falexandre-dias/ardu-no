fec0d48ac83ce8d4878a20dd99502af0afc76fd7
falexandre-dias <54296222+falexandre-dias@users.noreply.github.com>
2019-09-03 22:17:13 -0300

    Add files via upload

---
+++ Estudo_das_portas_logicas/Estudo_das_portas_logicas.ino
@@ -0,0 +1,15 @@
+/**
+   Estudo das portas lógicas
+   @author Fabio Dias
+*/void setup() {
+  pinMode (13, OUTPUT);
+  pinMode (2, INPUT);
+}
+
+void loop() {
+  if (digitalRead(2) == HIGH) {//se o botão estiver precionado condição ligado
+    digitalWrite (13, HIGH);
+  } else {
+    digitalWrite (13, LOW);//se o botão não estiver precionado condição desligado
+  }
+}
---
+++ Estudo_das_portas_logicas_if_else_and/Estudo_das_portas_logicas_if_else_and.ino
@@ -0,0 +1,15 @@
+/**
+   Estudo das portas lógicas
+   @author Fabio Dias
+*/void setup() {
+  pinMode (13, OUTPUT);
+  pinMode (2, INPUT);
+}
+
+void loop() {
+  if (digitalRead(2) == HIGH && digitalRead(3)==HIGH) {//Somonte se os dois botões estiverem pressionados funciona a máquina (AND ->&&)
+    digitalWrite (13, HIGH);
+  } else {
+    digitalWrite (13, LOW);//se o botão não estiver precionado condição desligado
+  }
+}
